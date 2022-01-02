76e765dc57f97be69ca2c686cc1aae8209899516
danieldamian09 <humbertorivero9@gmail.com>
2022-01-02 14:01:05 -0300

    obtener elementos por un ID

---
+++ obtenerElementosID.js
@@ -0,0 +1,10 @@
+// Selecionar un elemento por ID
+const title = document.getElementById('title');
+// console.log(title);
+
+// Cambiando el texto del elemento
+title.innerHTML = "Cursos";
+
+const description = document.getElementById('description');
+// console.log(description);
+description.innerHTML = "Listado de Cursos";
---
+++ window.js
@@ -0,0 +1,5 @@
+//En la consola podemos ver todas las funciones que tiene el objeto Window
+
+console.log(window)
+
+// Lo que mas nos interesa del Objeto Window para este curso es el atributo "document", el cual es otro objeto

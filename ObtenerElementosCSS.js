9ad1edd8016d722ebf7d978b3992e68438cb8e29
danieldamian09 <humbertorivero9@gmail.com>
2022-01-02 18:37:23 -0300

    elementos hijos

--- ObtenerElementosCSS.js
+++ ObtenerElementosCSS.js
@@ -1,21 +1,21 @@
 // Obtener un elemento por una regla css por medio de QuerySelector el cual va a retornar el primer elemento que cumpla con la regla
 
 const elementoQuerySelectorID = document.querySelector('#first-course');
-console.log(elementoQuerySelectorID);
+// console.log(elementoQuerySelectorID);
 
 const elementoQuerySelectorClass = document.querySelector('.list-group-item');
-console.log(elementoQuerySelectorClass);
+// console.log(elementoQuerySelectorClass);
 
 const elementoReglaEspecifica = document.querySelector("div.row > ul.list-group > li");
-console.log(elementoReglaEspecifica);
+// console.log(elementoReglaEspecifica);
 
 const itemsAll = document.querySelectorAll('.list-group-item');
-console.log(itemsAll);
+// console.log(itemsAll);
 
 // Regla para obtener todos los elementos LI inpares, es como una regla de CSS, para los pares puedo usar ('li:nth-child(odd)')
 
 const itemsPARES = document.querySelectorAll('li:nth-child(even)');
-console.log(itemsPARES);
+// console.log(itemsPARES);
 
 for (let i = 0; i < itemsPARES.length; i++) {
   let element = itemsPARES[i];
---
+++ elementosHijos.js
@@ -0,0 +1,21 @@
+// Como obtener los Hijos de un elemento en especial
+
+const list = document.querySelector('ul');
+// console.log(list)
+
+// Para saber cuanto hijos tiene un elemento
+// console.log(list.childElementCount)
+// Obtener el listado de Hijos
+console.log(list.children)
+// Al ser un listado podemos iterarlo
+for (let i = 0; i < list.children.length; i++) {
+  const elementHijos = list.children[i];
+  console.log(elementHijos)
+}
+
+// acceder a un hijo especifico de la lista
+console.log(list.children[3])
+// accerder al primer hijo
+console.log(list.firstElementChild)
+//  acceder al ultimo hijo
+console.log(list.lastElementChild.innerHTML)

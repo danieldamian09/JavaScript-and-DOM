7b59176b65cd0f3137336ea096b9faaf483f49af
danieldamian09 <humbertorivero9@gmail.com>
2022-01-03 19:48:50 -0300

    evento click

---
+++ eventoClick.js
@@ -0,0 +1,11 @@
+const buttonClick = document.querySelector('.btn.btn-primary');
+// console.log(buttonClick);
+
+//* buttonClick.addEventListener('click', function(){
+//*   console.log('Hola Mundo')
+//* })
+
+// Evento Doble Click
+buttonClick.addEventListener('dblclick', function(){
+  console.log('dobleclick');
+})

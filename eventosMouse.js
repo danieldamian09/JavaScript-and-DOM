cc5f566ab7e2ed90d4243c8510c3d53dfb5e32d1
danieldamian09 <humbertorivero9@gmail.com>
2022-01-04 14:36:53 -0300

    funcion setTimeOut

--- eventosMouse.js
+++ eventosMouse.js
@@ -1,5 +1,5 @@
 const buttonMouse = document.querySelector('.btn.btn-primary');
-console.log(buttonMouse);
+// console.log(buttonMouse);
 
 // Evento cuando el mouse esta sobre el boton
 buttonMouse.addEventListener('mouseenter', function(){
